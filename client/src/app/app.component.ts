4b5ebb439526d9cc1e9c220e6d9eb908514e0d56
Satya <satyap48@gmail.com>
2021-05-10 14:22:42 +0530

    Completion of walking skeleton - Angular

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    const url = 'https://localhost:5001/api/users';
+
+    this.http.get(url).subscribe(
+      (response) => {
+        this.users = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
